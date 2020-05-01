c5adc6a2f440a453c0cf1d33c34063f103f2709b
LaunDan <launadaniel2@gmail.com>
2020-05-01 14:16:26 +0200

    set java class on create
    xml for java class

--- draw/app/src/main/java/drawplace/p/DrawPlaceActivity.java
+++ draw/app/src/main/java/drawplace/p/DrawPlaceActivity.java
@@ -1,4 +1,20 @@
 package drawplace.p;
 
-public class DrawPlaceActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrawPlaceActivity extends AppCompatActivity {
+
+    private TextView coords;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_draw_place);
+
+        coords = findViewById(R.id.coord);
+    }
 }
+
