9f0f1caaa71764c203991b7b7eabe813f366c12c
LaunDan <launadaniel2@gmail.com>
2020-05-02 17:39:55 +0200

    change folder

---
+++ app/src/main/java/drawplace/p/DrawPlaceActivity.java
@@ -0,0 +1,23 @@
+package drawplace.p;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DrawPlaceActivity extends AppCompatActivity {
+
+    private TextView coords;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_draw_place);
+
+
+        coords = findViewById(R.id.coord);
+    }
+}
+
