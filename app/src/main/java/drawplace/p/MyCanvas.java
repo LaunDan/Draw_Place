fdfe5223093a153753e452ba834deea56fcc425c
LaunDan <launadaniel2@gmail.com>
2020-05-03 12:45:38 +0200

    created clas MyCanvas

---
+++ app/src/main/java/drawplace/p/MyCanvas.java
@@ -0,0 +1,13 @@
+package drawplace.p;
+
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.view.View;
+
+public class MyCanvas extends View {
+    private Paint paint = new Paint();
+    super.paint.setColor(Color.RED);
+
+
+
+}
