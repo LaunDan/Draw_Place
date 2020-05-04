eb40cb6bf57fc4f2554e944a49fe2e8d82f783d6
LaunDan <launadaniel2@gmail.com>
2020-05-04 10:22:56 +0200

    set class MyCanvas

--- app/src/main/java/drawplace/p/MyCanvas.java
+++ app/src/main/java/drawplace/p/MyCanvas.java
@@ -10,9 +10,14 @@ public class MyCanvas extends View {
 
     public MyCanvas(Context context) {
         super(context);
+        paint.setColor(Color.WHITE);
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setStrokeCap(Paint.Cap.ROUND);
+        paint.setStrokeJoin(Paint.Join.ROUND);
+        paint.setStrokeMiter(.5f);
+        paint.setStrokeWidth(5);
+
     }
-
-
-
 
 }
