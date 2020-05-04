6ba3ccc37539745d06106a45043ccd1cad9fdd4f
LaunDan <launadaniel2@gmail.com>
2020-05-04 21:20:29 +0200

    edit for update

--- app/src/main/java/drawplace/p/MyCanvas.java
+++ app/src/main/java/drawplace/p/MyCanvas.java
@@ -5,34 +5,34 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
+
+import java.util.ArrayList;
 
 public class MyCanvas extends View {
     private Paint paint = new Paint();
     private Path path = new Path();
+    private ArrayList<FingerPath> paths = new ArrayList<>();
 
     public MyCanvas(Context context) {
-        super(context);
+        this(context, null);
+    }
 
+    public MyCanvas(Context context, AttributeSet attrs) {
+        super(context, attrs);
         paint.setAntiAlias(true);
         paint.setStyle(Paint.Style.STROKE);
         paint.setStrokeCap(Paint.Cap.ROUND);
         paint.setStrokeJoin(Paint.Join.ROUND);
         paint.setStrokeMiter(.5f);
         paint.setStrokeWidth(5);
-
     }
-    //TODO change application to change path one by one
-
-    //TODO step back button
-
-    // TODO delete all button
-
-    // TODO change width of line
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        path = new Path();
         float xPos = event.getX();
         float yPos = event.getY();
 
@@ -53,6 +53,7 @@ public class MyCanvas extends View {
 
     @Override
     public void onDraw(Canvas canvas) {
+
         canvas.drawPath(path, paint);
     }
 
@@ -70,6 +71,4 @@ public class MyCanvas extends View {
             paint.setColor(Color.RED);
         }
     }
-
-
 }
