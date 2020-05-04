e2b080a1ed5d9870641257e733cec36f2c78965f
LaunDan <launadaniel2@gmail.com>
2020-05-04 21:19:26 +0200

    created new class FingerPaths

---
+++ app/src/main/java/drawplace/p/FingerPath.java
@@ -0,0 +1,18 @@
+package drawplace.p;
+
+import java.nio.file.Path;
+
+public class FingerPath {
+    public int color;
+    public Path path;
+    public int width;
+
+
+    public FingerPath(int color, Path path, int width) {
+        this.color = color;
+        this.path = path;
+        this.width = width;
+    }
+
+
+}
