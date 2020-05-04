60378d6982ac9838a0d01ad4e261f868055d4639
LaunDan <launadaniel2@gmail.com>
2020-05-04 10:34:51 +0200

    created method onTouchEvent

--- app/src/main/java/drawplace/p/MyCanvas.java
+++ app/src/main/java/drawplace/p/MyCanvas.java
@@ -3,10 +3,13 @@ package drawplace.p;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
+import android.view.MotionEvent;
 import android.view.View;
 
 public class MyCanvas extends View {
     private Paint paint = new Paint();
+    private Path path = new Path();
 
     public MyCanvas(Context context) {
         super(context);
@@ -18,6 +21,26 @@ public class MyCanvas extends View {
         paint.setStrokeMiter(.5f);
         paint.setStrokeWidth(5);
 
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        float xPos = event.getX();
+        float yPos = event.getY();
+
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                path.moveTo(xPos, yPos);
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+                path.lineTo(xPos, yPos);
+                break;
+
+        }
+        invalidate();
+        return true;
     }
 
 }
